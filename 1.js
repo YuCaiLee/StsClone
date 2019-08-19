936904e7115659c73af1c895087b4d4a96bc4e70
YuCaiLee <501834110@qq.com>
2019-08-19 16:53:49 +0800

    upload file
    upload file

---
+++ 1.js
@@ -0,0 +1,21 @@
+function load(){
+	var a='';
+	a='abcdefg'
+	a=a.slice(1,4);
+	alert(a);	
+}
+
+function load2(){
+	var a = new Array(1,2,3);
+	var b = new Array(1,2,3,);
+	alert(a.length+'-'+b.length)
+	
+}
+
+function load3(){
+	var a = '';
+	a='test';
+	a.a ='AA';
+	var v = a.a;
+	alert(v);
+}
